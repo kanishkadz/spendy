06a16a08b201df3d4e9a2330c15109dbc0caee30
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-17 18:56:49 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Root />} />
           <Route path="/login" exact element={<Login />} />
           <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
         </Routes>
       </Router>
     </div>
