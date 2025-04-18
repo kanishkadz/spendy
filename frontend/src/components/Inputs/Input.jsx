7c8013deefe480f2194a1d575697f5b9be0c8233
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-18 12:32:29 +0530

    Update Input.jsx

--- frontend/src/components/Inputs/Input.jsx
+++ frontend/src/components/Inputs/Input.jsx
@@ -14,6 +14,16 @@ const Input = ({value, onChange, placeholder, label, type}) => {
 
         <div className="input-box">
             <input type={type == 'password' ? showPassword ? 'text' : 'password' : type} placeholder={placeholder} className="w-full bg-transparent outline-none" value={value} onChange = {(e) => onChange(e)} />
+
+            {type === "password" && (
+                <>
+                    {showPassword ? (
+                        <FaRegEye size={22} className="text-primary cursor-pointer" onClick={() => toggleShowPassword()} />
+                    ) : (
+                        <FaRegEyeSlash size={22} className="text-slate-400 cursor-pointer" onClick={() => toggleShowPassword()} />
+                    )}
+                </>
+            )}
         </div>
     </div>
   )
