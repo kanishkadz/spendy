621001d3645fb27a3edc6036d28231e27cb4eaa7
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-18 11:49:40 +0530

    assets

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AuthLayout = ({ children }) => {
     return (
-        <div>
+        <div className="flex">
             <div className="w-screen h-screen md:w[60vw] px-12 pt-8 pb-12">
                 <h2 className="text-lg font-medium text-black">Spendy</h2>
                 {children}
