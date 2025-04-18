f56d2d290cf733aca390a0867ae424059707d828
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-18 11:56:57 +0530

    Update AuthLayout.jsx

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -11,7 +11,7 @@ const AuthLayout = ({ children }) => {
 
             <div className="hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
                 <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5" />
-                <div className="" />
+                <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-10" />
                 <div className="" />
 
                 <img src={Banner} alt="" />
