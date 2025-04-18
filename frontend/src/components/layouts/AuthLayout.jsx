474919777261d0c0a64b9412e12a081570d18bff
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-18 12:11:51 +0530

    Update AuthLayout.jsx

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -25,4 +25,16 @@ const AuthLayout = ({ children }) => {
     )
 }
 
-export default AuthLayout
+export default AuthLayout;
+
+const StatsInfoCard = ({icon, label, value, color}) => {
+    return <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/50 z-10">
+        <div className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>
+            {icon}
+        </div>
+        <div>
+            <h6 className="text-xs text-gray-500 mb-1">{label}</h6>
+            <span className="text-[20px]">${value}</span>
+        </div>
+    </div>
+}
