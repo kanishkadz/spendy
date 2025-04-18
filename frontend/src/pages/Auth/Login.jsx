e804ba7d44d844c427710d27f30714620d3dde3e
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-18 12:18:02 +0530

    Update Login.jsx

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -8,13 +8,17 @@ const Login = () => {
   const [error, setError] = useState(null);
 
   const navigate = useNavigate();
+  const handleLogin = async (e) => {};
+
   return (
     <AuthLayout>
       <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">Please enter your details to log in.</p>
 
-
+        <form onSubmit={handleLogin}>
+          <Input value={email} onChange = {({ target }) => setEmail(target.value)} label="Email Address" placeholder="Enter your email" type="text" />
+        </form>
       </div>
     </AuthLayout>
   )
