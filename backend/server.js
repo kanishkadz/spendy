e9f2fb812293681677682d474434416240ab2fe1
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-20 19:10:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const { connect } = require("http2");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
