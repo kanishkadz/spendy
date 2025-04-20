fea76dcaa839d0342848286faa75bfd1d77a7e71
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-20 15:36:46 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,7 +7,35 @@ const generateToken = (id) => {
 };
 
 //register user
-exports.registerUser = async (req, res) => {};
+exports.registerUser = async (req, res) => {
+    const {fullName, email, password, profileImageUrl} = req.body;
+
+    //to check for missing fields
+    if (!fullName || !email || !password) {
+        return res.status(400).json({message: "All fields are required!"});
+    }
+
+    try{
+        //check if email is already registered
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "Email already in use!"});
+        }
+
+        //create user
+        const user = await User.create({
+            fullName, email, password, profileImageUrl,
+        });
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch (err) {
+        res.status(500).json({message: "Error registering user!", error: err.message});
+    }
+};
 
 //login user
 exports.loginUser = async (req, res) => {};
