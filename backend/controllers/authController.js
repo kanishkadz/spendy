37d3b27586bf3dd22268b0b0a455433a79573bbe
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-20 18:48:39 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -61,4 +61,16 @@ exports.loginUser = async (req, res) => {
 };
 
 //register user
-exports.getUserInfo = async (req, res) => {};
+exports.getUserInfo = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-passowrd");
+
+        if (!user) {
+            res.status(400).json({message: "User not found"});
+        }
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({message: "Error registering user!", error: err.message});
+    }
+};
