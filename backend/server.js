4b54d4507c662f6ad3540075da18cb75c3211343
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-20 15:13:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require("path");
 const { connect } = require("http2");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -18,6 +19,8 @@ app.use(
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
