c968a4ba3e21dce79709d8084e4f0340373deed2
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-20 21:40:02 +0530

    Update incomeController.js

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -2,7 +2,28 @@ const User = require("../models/User");
 const Income = require("../models/Income");
 
 //add income source
-exports.addIncome = async (req, res) => {};
+exports.addIncome = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        if (!source || !amount || !date) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        const newIncome = new Income({
+            userId,
+            icon, 
+            source,
+            amount,
+            date: new Date(date)
+        });
+
+        await newIncome.save();
+        res.status(200).json(newIncome);
+    } catch (err) {
+        res.status(500).json({message: "Server Error"});
+    }
+};
 
 //get all income source
 exports.getAllIncome = async (req, res) => {};
