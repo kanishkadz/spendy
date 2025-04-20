9b91829783b53d554104ccacc303bad9326805bc
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-20 21:51:34 +0530

    Update incomeController.js

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -50,4 +50,25 @@ exports.deleteIncome = async (req, res) => {
 };
 
 //download excel
-exports.downloadIncomeExcel = async (req, res) => {};
+exports.downloadIncomeExcel = async (req, res) => {
+    const userId = req.user.id;
+
+    try {
+        const income = await Income.find({userId}).sort({date: -1});
+
+        //prepare data for excel
+        const data = income.map((item) => ({
+            Source: item.source,
+            Amount: item.amount,
+            Date: item.date,
+        }));
+
+        const wb = xlsx.utils.book_new();
+        const ws = xlsx.utils.json_to_sheet();
+        xlsx.utils.book_append_sheet(wb, ws, "Income");
+        xlsx.writeFile(wb, 'income_details.xlsx');
+        res.download('income_details.xlsx');
+    } catch (err) {
+        res.status(500).json({message: "Server Error"});
+    }
+};
