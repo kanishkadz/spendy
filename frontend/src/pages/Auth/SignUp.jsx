068f9e44c7732b19aa3a967ba6bd176ca212050e
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-21 21:48:09 +0530

    Update SignUp.jsx

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -7,6 +7,7 @@ import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import { UserContext } from '../../context/userContext';
+import uploadImage from '../../utils/uploadImage';
 
 const SignUp = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -48,7 +49,7 @@ const SignUp = () => {
         profileImageUrl = imgUploadRes.imageUrl || "";
       }
       const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
-        fullName, email, password,
+        fullName, email, password, profileImageUrl,
       });
 
       const {token, user} = response.data;
