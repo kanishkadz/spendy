27aef391d4983182ce7d0c779c804813d79e7f19
Kanishka Anand <kanishkaadz@gmail.com>
2025-04-21 22:17:08 +0530

    Update Navbar.jsx

--- frontend/src/components/layouts/Navbar.jsx
+++ frontend/src/components/layouts/Navbar.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {HiOutlineMenu, HiOutlineX} from "react-icons/hi";
+import SideMenu from './SideMenu';
 
-const Navbar = () => {
+const Navbar = ({activeMenu}) => {
+    const [openSideMenu, setOpenSideMenu] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <div className='flex gap-5 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
+        <button className='' onClick={() => {setOpenSideMenu(!openSideMenu);}}>
+            {openSideMenu ? (
+                <HiOutlineX className='' />
+            ) : (
+                <HiOutlineMenu className='' />
+            )}
+        </button>
+
+        <h2 className=''>Spendy</h2>
+
+        {openSideMenu && (
+            <div className=''>
+                <SideMenu activeMenu={activeMenu} />
+            </div>
+        )}
+    </div>
   )
 }
 
